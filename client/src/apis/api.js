c29e7f9ba372f7da9e1f9ac8a06d9c324ee8b651
Prithviraj Bhavsar <75017929+PrithvirajBhavsar@users.noreply.github.com>
2021-04-13 23:37:09 +0530

    Delete client directory

--- client/src/apis/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const url = "http://localhost:5000/";
-
-export const getUser = ()=> axios.get(url);
-export const postUser = (user)=> axios.post(url,user);
-export const postShop = (shop)=> axios.post(`${url}openshop`,shop);
-export const getShop = ()=> axios.get(`${url}openShop`);
--- client/src/reducers/combineReducers.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from "redux";
-
-import apiReducer from "./apiReducer";
-import stateReducer from "./stateReducer";
-import openShopReducer  from "./openShopReducer"
-
-export default combineReducers({apiReducer,stateReducer,openShopReducer});
